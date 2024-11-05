231b1cc0b3e097dca6c4653d58bab46c4a5eac71
Saxanth <saxanth@gmail.com>
2024-11-05 13:43:36 -0600

    improved the naming conventions for the configuration, and generative model abstract classes..

--- apps/services/generation.py
+++ apps/services/generation.py
@@ -1,29 +1,28 @@
-from services import *
 from abc import ABC, abstractmethod
 from enum import Enum
 from ell import BaseModel, Field, Dict, Any
 
-class AIGenerativeType(Enum):
-    NONE    = 0
-    AUDIO   = 1
-    CODE    = 2
-    IMAGE   = 3
-    TEXT    = 4
-    VIDEO   = 5
+class GenerativeModelType(Enum):
+    NONE    = 0 # ignored
+    AUDIO   = 1 # audio generation
+    CODE    = 2 # code  generation
+    IMAGE   = 3 # image generation
+    TEXT    = 4 # text  generation
+    VIDEO   = 5 # video generation 
 
-class GenerativeAIConfig(BaseModel):    
-    name:       str = Field(description="The name of the generative AI model to use",   default="")
-    api_key:    str = Field(description="The API key for the generative AI service",    default="")
-    api_base:   str = Field(description="The base URL for the generative AI service",   default="")
-    type: AIGenerativeType  = Field(description="The type of generative AI model to use", default=AIGenerativeType.NONE)
+class ModelConfig(BaseModel):
+    api_key:  str  = Field(description="The API key for the generative AI service",    default="")
+    api_base: str  = Field(description="The base URL for the generative AI service",   default="")    
+    api_args: Dict = Field(description="A dictionary of values representing the arguments to use for the generative model", default={"model" : ''})
+    type: GenerativeModelType  = Field(description="The type of generative AI model to use", default=GenerativeModelType.NONE)
 
-class GenerativeAIBaseClass(ABC):
-    def __init__(self, config: GenerativeAIConfig):
-        self.__configuration__: GenerativeAIConfig = config
+class GenerativeModel(ABC):
+    def __init__(self, config: ModelConfig):
+        self.__configuration__: ModelConfig = config
 
     @property
-    def get_config(self) -> GenerativeAIConfig: return self.__configuration__
+    def get_config(self) -> ModelConfig: return self.__configuration__.model_copy()
 
     @abstractmethod
-    async def generate(self, prompt: str, additional_settings: list[str] = []) -> any:        
+    async def generate(self, prompt: str, additional_settings: Dict[str, Any] = []) -> Any:
         raise NotImplementedError("This method must be implemented in a subclass")
