c337972e9acd6a12512b10409627f0f5df6c430f
Saxanth <saxanth@gmail.com>
2024-11-05 16:04:12 -0600

    Flux is now generating images.

--- apps/app.py
+++ apps/app.py
@@ -2,28 +2,37 @@ import ollama
 from services.models import NomicModel, CodestralModel, FluxModel
 
 def main():
-    model_list = { item['name'] for item in ollama.list()['models'][:-1] }
-    print ("Ollama Models:\n" + '\n'.join(model_list))
+    # we have problems loading both the 'ollama' models and the 'flux' fp model.
+    # so if you want to test 'image_gen' then set this value to true
+    enable_image_gen = False
 
-    prompt = "a simple python script to print a range of integers in the console."
-    result = CodestralModel.generate_code(
-        user_message=prompt,
-        api_params={
-            "model": 'codestral:latest',
-            "temperature": 0.9,
-            "max_tokens": 100_000,                
-            "top_p": 0.9185, 
-            "presence_penalty": 0.6, 
-            "frequency_penalty": 1.2
-        }
-    )[0]
-    print (f"\nCode Result:\n{result}")
+    if not enable_image_gen:
+        model_list = { item['name'] for item in ollama.list()['models'][:-1] }
+        print ("Ollama Models:\n" + '\n'.join(model_list))
 
-    prompt = "Hello World!"
-    result = NomicModel.generate_embedding("hello world")
-    print (f'\nEmbeddingResult:\n{result}')
+        prompt = "a simple python script to print a range of integers in the console."
+        result = CodestralModel.generate_code(
+            user_message=prompt,
+            api_params={
+                "model": 'codestral:latest',
+                "temperature": 0.9,
+                "max_tokens": 100_000,                
+                "top_p": 0.9185, 
+                "presence_penalty": 0.6, 
+                "frequency_penalty": 1.2
+            }
+        )[0]
+        print (f"\nCode Result:\n{result}")
 
-    prompt = """ a cute kitten holding a sign which reads "I'm a cute kitten" """
-    result = FluxModel.generate_image(prompt)
+        prompt = "Hello World!"
+        result = NomicModel.generate_embedding("hello world")
+        print (f'\nEmbeddingResult:\n{result}')
+
+    # generate image using flux, and custom long version clip model. 
+    # Need to possibly change over to flan-t5-xxl-gguf for the clip model | or possibly Qwen2.5??
+    # Need to possibly change over the fp-8 for the diffusion model.
+    else:
+        prompt = """ a cute kitten holding a sign which reads "I'm a cute kitten" """
+        FluxModel.generate_image(prompt) # loads the full model into gpu... probably best to test vram size, and quantize if necessary....
 
 if __name__ == "__main__": main()
--- apps/services/models/image/flux.py
+++ apps/services/models/image/flux.py
@@ -1,8 +1,57 @@
+from typing import Literal
+import torch, os, uuid, tqdm
+from huggingface_hub import login
+from diffusers import FluxPipeline
+from transformers import CLIPModel, CLIPProcessor, CLIPConfig
+
+import warnings
+warnings.simplefilter("ignore") # Stop spam of future warnings I'm seeing
+
+login('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')
+MODEL_NAME = 'Flux.1-dev'
 
 class FluxModel():
-    def __init__(self) -> None:
+    def __init__(self, model: Literal['Flux.1-dev'] = 'Flux.1-dev') -> None:
+        MODEL_NAME = model
         pass
 
     @staticmethod
     def generate_image(prompt: str):
-        pass
+        image_folder = os.path.abspath('./apps/services/models/.cache/images')
+        if not os.path.exists(image_folder) : os.makedirs(image_folder)
+
+        model_id = "zer0int/LongCLIP-GmP-ViT-L-14"
+        config = CLIPConfig.from_pretrained(model_id)
+        maxtokens = 248
+
+        config.text_config.max_position_embeddings = maxtokens
+
+        clip_model = CLIPModel.from_pretrained(model_id, torch_dtype=torch.bfloat16, config=config, device_map="balanced")
+        clip_processor = CLIPProcessor.from_pretrained(model_id, padding="max_length", max_length=maxtokens, return_tensors="pt", truncation=True)
+
+        pipe = FluxPipeline.from_pretrained(f"black-forest-labs/{MODEL_NAME}", torch_dtype=torch.bfloat16, device_map="balanced")
+        pipe.set_progress_bar_config(disable=True)
+
+        # Set the custom tokenizer and text encoder from your CLIP model
+        pipe.tokenizer = clip_processor.tokenizer   # Replace with the custom CLIP tokenizer
+        pipe.text_encoder = clip_model.text_model   # Replace with the custom CLIP text encoder
+        pipe.tokenizer_max_length = maxtokens       # Long-CLIP token max
+        pipe.text_encoder.dtype = torch.bfloat16    # Ensure the text encoder uses bfloat16
+
+        pipe.vae.enable_slicing()
+        pipe.vae.enable_tiling()
+
+        seed:int = torch.randint(low=1337, high=61337, size=[1])[0].item()
+        rand_generator = torch.Generator("cuda").manual_seed( seed )    
+
+        image = pipe( 
+            prompt=prompt,
+            height=384,
+            width=680,
+            guidance_scale=3.5,
+            num_inference_steps=16,
+            max_sequence_length=maxtokens,
+            generator=rand_generator,
+        ).images[0]
+
+        image.save(f"{image_folder}\\{ str(uuid.uuid4()) }.png")
